6d79e2991e237095a39fe948e018ff5aff6fff3b
Isaac Jon <ijon@stuy.edu>
2019-05-15 21:47:14 -0400

    register works

--- utils/database.py
+++ utils/database.py
@@ -1,4 +1,5 @@
 import sqlite3
+import random
 
 dbfile = "data/userdata.db"
 
@@ -24,7 +25,7 @@ def checkuser(user):
     db = initdb()
     c = db.cursor()
 
-    c.execute("SELECT * FROM users WHERE name = ?", (user, ))
+    c.execute("SELECT * FROM users WHERE username = ?", (user, ))
     dupusers = c.fetchall()
 
     db.close()
@@ -35,7 +36,7 @@ def getpassword(user):
     db = initdb()
     c = db.cursor()
 
-    c.execute("SELECT password FROM users WHERE name = ?", (user, ))
+    c.execute("SELECT password FROM users WHERE username = ?", (user, ))
     password = c.fetchone()[0]
 
     db.close()
@@ -46,7 +47,7 @@ def resetpassword(user, newpass):
     db = initdb()
     c = db.cursor()
 
-    c.execute("UPDATE users SET password = ? WHERE name = ?", (newpass, user))
+    c.execute("UPDATE users SET password = ? WHERE username = ?", (newpass, user))
 
     db.commit()
     db.close()
@@ -55,7 +56,7 @@ def loginuser(user, password):
     db = initdb()
     c = db.cursor()
 
-    c.execute("SELECT * FROM users WHERE name = ? AND password = ?", (user, password))
+    c.execute("SELECT * FROM users WHERE username = ? AND password = ?", (user, password))
     creds = c.fetchall()
 
     db.close()
@@ -63,119 +64,121 @@ def loginuser(user, password):
     return len(creds) > 0
 
 def newuser(user, password):
+    randName = random.randint(0, 200)
+
     db = initdb()
     c = db.cursor()
 
-    c.execute("INSERT INTO users VALUES(?,?,?,?,?)", (user, password, 20000, "", ""))
+    c.execute("INSERT INTO users VALUES(?,?,?,?)", (user, password,str(randName), 0))
 
     db.commit()
     db.close()
 
     return True
 
-def changechips(user, newchips):
+def changecoins(user, newcoins):
     db = initdb()
     c = db.cursor()
 
-    c.execute("UPDATE users SET chips = ? WHERE name = ?", (newchips, user))
+    c.execute("UPDATE users SET coins = ? WHERE username = ?", (newcoins, user))
 
     db.commit()
     db.close()
 
-def formatmatchdata(endchips, rank, time):
-    return endchips + ":" + rank + ":" + time + ";"
-
-def addpastmatch(user, match):
+def fetchcoins(user):
     db = initdb()
     c = db.cursor()
 
-    c.execute("SELECT match_history FROM users WHERE name = ?", (user, ))
-    match_history = c.fetchone()[0]
-    if match_history == None or match_history == "":
-        c.execute("UPDATE users SET match_history = ? WHERE name = ?", (match, user))
-    else:
-        c.execute("UPDATE users SET match_history = ? WHERE name = ?", (match + match_history, user))
-
-    db.commit()
-    db.close()
-
-def deformatmatch(match):
-    return match.split(":")
-
-def getpastmatches(user):
-    db = initdb()
-    c = db.cursor()
-
-    c.execute("SELECT match_history FROM users WHERE name = ?", (user, ))
-    match_history = c.fetchone()[0]
-
-    if match_history == "" or match_history == None:
-        return ""
-    else:
-        match_history = match_history.split(";")
-
-    while match_history[-1] == "":
-        match_history = match_history[0:-1]
-
-    for i in range(len(match_history)):
-        currmatch = deformatmatch(match_history[i])
-        match_history[i] = {"time": currmatch[2], "chips": int(currmatch[0]), "rank": int(currmatch[1])}
+    c.execute("SELECT coins FROM users WHERE username = ?", (user, ))
+    coins = c.fetchone()[0]
 
     db.close()
 
-    return match_history
+    return coins
 
-def formatcurrdata(chips, playername):
-    return chips + ":" + playername
-
-def addcurrmatch(user, match):
-    db = initdb()
-    c = db.cursor()
-
-    c.execute("UPDATE users SET current_games = ? WHERE name = ?", (match, user))
-
-    db.commit()
-    db.close()
-
-def readcurrmatch(user):
-    db = initdb()
-    c = db.cursor()
-
-    c.execute("SELECT current_games FROM users WHERE name = ?", (user, ))
-    currmatch = c.fetchone()[0]
-
-    db.close()
-
-    return currmatch
-
-def checkcurrmatch(user):
-    db = initdb()
-    c = db.cursor()
-
-    c.execute("SELECT current_games FROM users WHERE name = ?", (user, ))
-    currmatch = c.fetchone()[0]
-    rtrnval = (currmatch != "" and currmatch != None)
-
-    db.close()
-
-    return rtrnval
-
-def clearcurrmatch(user):
-    db = initdb()
-    c = db.cursor()
-
-    c.execute("UPDATE users SET current_games = ? WHERE name = ?", ("", user))
-
-    db.commit()
-    db.close()
-
-def fetchchips(user):
-    db = initdb()
-    c = db.cursor()
-
-    c.execute("SELECT chips FROM users WHERE name = ?", (user, ))
-    chips = c.fetchone()[0]
-
-    db.close()
-
-    return chips
+# def formatmatchdata(endchips, rank, time):
+#     return endchips + ":" + rank + ":" + time + ";"
+#
+# def addpastmatch(user, match):
+#     db = initdb()
+#     c = db.cursor()
+#
+#     c.execute("SELECT match_history FROM users WHERE username = ?", (user, ))
+#     match_history = c.fetchone()[0]
+#     if match_history == None or match_history == "":
+#         c.execute("UPDATE users SET match_history = ? WHERE name = ?", (match, user))
+#     else:
+#         c.execute("UPDATE users SET match_history = ? WHERE name = ?", (match + match_history, user))
+#
+#     db.commit()
+#     db.close()
+#
+# def deformatmatch(match):
+#     return match.split(":")
+#
+# def getpastmatches(user):
+#     db = initdb()
+#     c = db.cursor()
+#
+#     c.execute("SELECT match_history FROM users WHERE name = ?", (user, ))
+#     match_history = c.fetchone()[0]
+#
+#     if match_history == "" or match_history == None:
+#         return ""
+#     else:
+#         match_history = match_history.split(";")
+#
+#     while match_history[-1] == "":
+#         match_history = match_history[0:-1]
+#
+#     for i in range(len(match_history)):
+#         currmatch = deformatmatch(match_history[i])
+#         match_history[i] = {"time": currmatch[2], "chips": int(currmatch[0]), "rank": int(currmatch[1])}
+#
+#     db.close()
+#
+#     return match_history
+#
+# def formatcurrdata(chips, playername):
+#     return chips + ":" + playername
+#
+# def addcurrmatch(user, match):
+#     db = initdb()
+#     c = db.cursor()
+#
+#     c.execute("UPDATE users SET current_games = ? WHERE name = ?", (match, user))
+#
+#     db.commit()
+#     db.close()
+#
+# def readcurrmatch(user):
+#     db = initdb()
+#     c = db.cursor()
+#
+#     c.execute("SELECT current_games FROM users WHERE name = ?", (user, ))
+#     currmatch = c.fetchone()[0]
+#
+#     db.close()
+#
+#     return currmatch
+#
+# def checkcurrmatch(user):
+#     db = initdb()
+#     c = db.cursor()
+#
+#     c.execute("SELECT current_games FROM users WHERE name = ?", (user, ))
+#     currmatch = c.fetchone()[0]
+#     rtrnval = (currmatch != "" and currmatch != None)
+#
+#     db.close()
+#
+#     return rtrnval
+#
+# def clearcurrmatch(user):
+#     db = initdb()
+#     c = db.cursor()
+#
+#     c.execute("UPDATE users SET current_games = ? WHERE name = ?", ("", user))
+#
+#     db.commit()
+#     db.close()
