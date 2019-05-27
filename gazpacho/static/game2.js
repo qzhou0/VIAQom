aa8e4d9d022cbc4c1963cecbef1cf6e9ad53a9e9
Qian Zhou <qzhou@stuy.edu>
2019-05-27 14:29:50 -0400

    some changes...

--- gazpacho/static/game2.js
+++ gazpacho/static/game2.js
@@ -4,26 +4,18 @@ var ctx = c.getContext("2d");
 //var stop = document.getElementById("stop");
 
 //variables
-var blocks=9;
+var blocks=15;
 var numrows=20;
 var radius = 10;
 var block_fertility=.3;
+var maxHP=12;//max hp at birth
+var blockGrowth=.34;//value of HP added each time it falls down
 
 var loss = false;
 var id;
 
-var growing = true;
-var xVel = 0;
-var yVel = 10;
-var ballX = c.width/2;
-var ballY = c.height/2;
-var isDead = false;
 var canHitX = true;
 var canHitY = true;
-
-var blockid=0;
-
-
 
 var width=c.width;
 var height=c.height;
@@ -31,8 +23,30 @@ var height=c.height;
 var rectWidth = width/blocks;
 var rectHeight =height/numrows;
 
-var rects = []//[{'x':70,'y':20,'hp':2, 'hit':false},{'x':170,'y':20,'hp':1, 'hit':false}]
+var balls=[]
+var ballid=1;
+var blockid=0;
+var rects = []//[{'id':,x':70,'y':20,'hp':2, 'hit':false},{'x':170,'y':20,'hp':1, 'hit':false}]
 var rectCoors=[]
+
+var addBall=function(n){
+    while (n>0){
+	b2={'x':c.width/4,
+	    'y':c.height/2,
+	    'xVel':0,
+	    'yVel':10,
+	    'isDead':false,
+	    'id':ballid
+	   };
+	balls.push(b2);
+	n-=1;
+	ballid+=1;
+    }
+	
+}
+addBall(5);
+
+var liveCount=balls.length;
 
 var grid = function(){
     //clear();
@@ -48,6 +62,7 @@ var down=function(){
     for (i=0;i<rects.length;i++){
 	rects[i]['y']+=rectHeight;
 	rectCoors[i][1]+=rectHeight;
+	rects[i]['hp']+=blockGrowth;
     }
 };
 var newRow=function(range=1){//each time this is called, refreshes board with the addition of boards in RANGE rows, and all other rows move down
@@ -61,10 +76,10 @@ var newRow=function(range=1){//each time this is called, refreshes board with th
 		if (Math.random()<rate){
 		    d={};
 		    d['id']=blockid;
-		    d['hp']=1+ Math.floor(2*Math.random());
+		    d['hp']=1+ Math.floor(maxHP*Math.random());
 		    d['x']=i*rectWidth;
 		    d['y']=j*rectHeight;
-
+		    d['points']=parseInt(Math.random()*20);
 		    d['hit']=false;
 		    countadded++;
 		    blockid++;
@@ -91,8 +106,12 @@ var dot=function(x,y){
     ctx.fill();
     ctx.beginPath();
 }
-var coll = function(rect){
+var coll = function(rect,ball){
     // y collide
+    xVel=ball['xVel']
+    yVel=ball['yVel']
+    ballX=ball['x']
+    ballY=ball['y']
     var epsilonX=Math.abs(xVel);
     var epsilonY=Math.abs(yVel);
     // dot(rect['x']-radius,rect['y']-radius);
@@ -106,11 +125,11 @@ var coll = function(rect){
 	    ballX<rect['x']+rectWidth+radius+epsilonX){
 
 	    //yVel*=-1;
-      if(canHitX){
-	    xVel*=-1;
-      }
+	    if(canHitX){
+		ball['xVel']*=-1;
+	    }
 	    rect['hit'] = true;
-      canHitX = false;
+	    canHitX = false;
 	    circles.push([ballX,ballY]);
 	    return true;
 	}
@@ -122,12 +141,12 @@ var coll = function(rect){
 	 ballY<rect['y']+rectHeight+radius+epsilonY){
 	    //xVel*=-1;
       if(canHitY){
-	       yVel*=-1;
+	       ball['yVel']*=-1;
       }
 	    circles.push([ballX,ballY]);
 
 	    rect['hit']=true;
-      canHitY = false;
+	    canHitY = false;
 	    return true;
 	}
     }
@@ -151,83 +170,97 @@ var dvdLogoSetup = function(){
 	window.cancelAnimationFrame(id);
 	clear();
 
-
-	ballX += xVel;
-	ballY += yVel;
-  canHitX = true;
-  canHitY = true;
-	//ctx.fillRect( ballX, ballY, rectWidth, rectHeight);
-	//ctx.fillRect(ballX, ballY, rectWidth, rectHeight)
-	ctx.fillStyle = "red";
-	toRemove=[]
-	for(i=0; i<rects.length; i++){
-      
-	    block = rects[i];
-	    //if(block["hp"]>0){
-	    if (block['hp']<=0){
-		rects.pop(i);
-		rectCoors.pop(i);
-		continue;
-	    }
-	    else if(coll(block)){
-		ctx.fillStyle = "green";
-		console.log('hp',block['hp'],'xy',block['x'],block['y']);
-		block["hp"]--;
-		console.log(block['id'], ' hit!');
-	    }
-	    hp=block['hp'];
-	    if(hp>0){
-		if (hp==1){
-		    ctx.fillStyle='red';
+	for (b=0;b<balls.length;b++){
+	    ball=balls[b];
+	    ball['x'] += ball['xVel'];
+	    ball['y']+= ball['yVel'];
+	    canHitX = true;
+	    canHitY = true;
+	    //ctx.fillRect( ballX, ballY, rectWidth, rectHeight);
+	    //ctx.fillRect(ballX, ballY, rectWidth, rectHeight)
+	    ctx.fillStyle = "red";
+	    toRemove=[]
+	    for(i=0; i<rects.length; i++){
+		
+		block = rects[i];
+		//if(block["hp"]>0){
+		if (block['hp']<=0){
+		    rects.pop(i);
+		    rectCoors.pop(i);
+		    continue;
 		}
-		else if (hp==2){
-		    ctx.fillStyle='purple';
+		else if(coll(block,ball)){
+		    ctx.fillStyle = "green";
+		    console.log('hp',block['hp'],'xy',block['x'],block['y']);
+		    block["hp"]--;
+		    console.log(block['id'], ' hit!');
+		}
+		hp=block['hp'];
+		if(hp>0){
+		    if (hp==0){
+			ctx.fillStyle='green';
+		    }
+		    else if(hp<=1){
+			ctx.fillStyle='orange';
+		    }
+		    else if (hp<4){
+			ctx.fillStyle='red';
+		    }
+		    else if (hp<5){
+			ctx.fillStyle='purple';
+		    }
+		    else{
+			ctx.fillStyle='black';
+		    }
+		    ctx.fillRect(block["x"],block["y"], rectWidth, rectHeight);
 		}
 		else{
-		    ctx.fillStyle='black';
+		    ctx.fillRect(block["x"],block["y"], rectWidth, rectHeight);
+		    toRemove.push(i);
+		    
 		}
-		ctx.fillRect(block["x"],block["y"], rectWidth, rectHeight);
+		while (toRemove.length!=0){
+		    var j=toRemove.pop();
+		    rects.splice(j,1);
+		    rectCoors.splice(j,1);
+		}
 	    }
-	    else{
-		ctx.fillRect(block["x"],block["y"], rectWidth, rectHeight);
-		toRemove.push(i);
+	
+	}
 
-	    }
-	    //}
-	}
-	while (toRemove.length!=0){
-	    var j=toRemove.pop();
-	    rects.splice(j,1);
-	    rectCoors.splice(j,1);
-	}
 	//console.log('xvel',xVel,'yvel',yVel);
 
 
+	for (b=0;b<balls.length;b++){
+	    ball=balls[b]
+	    ctx.fillStyle = "blue";
+	    ctx.beginPath();
+	    ctx.arc(ball['x'],ball['y'],radius,0,2*Math.PI);
+	    ctx.stroke();
+	    ctx.fill();
 
-	ctx.fillStyle = "blue";
-	ctx.beginPath();
-	ctx.arc(ballX,ballY,radius,0,2*Math.PI);
-	ctx.stroke();
-	ctx.fill();
 
 
-
-	if(ballX - radius <= 0 || ballX + radius >= c.width){
-	    xVel *= -1;
-	}
-	else if(ballY - 2*radius <= 0){
-	    yVel *= -1;
-	}
-	else if(ballY+radius+yVel>= c.height){
-	    if (! isDead){
-		newRow();
-		console.log(rects);
+	    if(ball['x'] - radius <= 0 || ball['x'] + radius >= c.width){
+		ball['xVel'] *= -1;
 	    }
-	    isDead = true;
-	    xVel = 0;
-	    yVel = 0;
+	    else if(ball['y'] - 2*radius <= 0){
+		ball['yVel'] *= -1;
+	    }
+	    else if(ball['y']+radius+ball['yVel']>= c.height){
+		if (liveCount==1 && !ball['isDead']){
+		    newRow();
+		    console.log(rects);
+		}
+		if (!ball['isDead']){
+		    liveCount-=1;
+		}
+		ball['isDead'] = true;
 
-
+		//console.log(liveCount);
+		ball['xVel'] = 0;
+		ball['yVel'] = 0;
+	    }
 	}
 
 	//for (i=0;i<circles.length;i++){
@@ -243,17 +276,22 @@ var dvdLogoSetup = function(){
 
 c.addEventListener("click",function(e){
     //console.log('click',xVel , ":x,y:" , yVel);
-    if(isDead){
-
-	xVel = e.offsetX - ballX;
-	yVel = e.offsetY - ballY;
-	var ratio = Math.sqrt(100/(xVel*xVel + yVel*yVel));
-	//console.log(xVel + ":x,y:" + yVel);
-	xVel *= ratio;
-	yVel *= ratio;
-	//console.log(xVel + ":x,y: " + yVel);
-	isDead = false;
+    if(liveCount==0){
+	for (b=0;b<balls.length;b++){
+	    ball=balls[b];
+	    ball['xVel'] = e.offsetX - ball['x'];
+	    ball['yVel'] = e.offsetY - ball['y'];
+	    var ratio = Math.sqrt(100/(ball['xVel']*ball['xVel'] + ball['yVel']*ball['yVel']));
+	    //console.log(xVel + ":x,y:" + yVel);
+	    ball['xVel'] *= ratio;
+	    ball['yVel'] *= ratio;
+	    //console.log(xVel + ":x,y: " + yVel);
+	    ball['isDead'] = false;
+	    
+	}
+	liveCount=balls.length;
     }
+    
 });
 
 //stop
@@ -264,3 +302,10 @@ var stopIt=function(){
 //stop.addEventListener('click',stopIt);
 
 start.addEventListener("click", dvdLogoSetup);
+
+var ballsDown=function(){
+    for (b=0;b<balls.length;b++){
+	balls[b]['y']=height-radius;
+	balls[b]['x']=width/2;
+    }
+}
